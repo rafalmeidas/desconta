5e3569a50c2daaeaf9b2da5941e0217fb7ca6c03
Rafael Almeida <supremotop10@gmail.com>
2019-09-06 00:35:18 -0300

    ADD CompraFormRequest

---
+++ app/Http/Requests/CompraFormRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CompraFormRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'data_venda' => 'required|min:3|max:45',
+            'qtde_parcelas' => 'required|min:3|max:45',
+            'valor_total' => 'required|max:45',
+            'empresa_id' => 'required|min:1|exists:empresas,id',
+            'pessoa_id' => 'required|min:1|exists:pessoas,id'
+        ];
+    }
+}
