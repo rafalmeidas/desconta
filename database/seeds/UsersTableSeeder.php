d783ab95467a86e52692f2e4f2688ebb43799309
Rafael Almeida <supremotop10@gmail.com>
2019-10-23 01:01:41 -0300

    ADD novas empresas e novos usuários

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -6,23 +6,14 @@ use App\User;
 class UsersTableSeeder extends Seeder {
 
     public function run() {
-        User::create([
-            'name' => 'Rafael Almeida',
-            'email' => 'rafael@gmail.com',
-            'password' => bcrypt('123'),
-            'empresa_id' => 2,
-            'tipo_login' => 'Administrador', 
-            'status' => true
-        ]);
-        
-        User::create([
-            'name' => 'Usuário',
-            'email' => 'u@g.com',
-            'password' => bcrypt('123'),
-            'empresa_id' => 1,
-            'tipo_login' => 'Usuário', 
-            'uid_firebase' => '',
-            'status' => true
+
+        DB::table('users')->delete();
+
+        DB::table('users')->insert([
+            [ 'name' => 'Usuário', 'email' => 'u@g.com', 'password' => bcrypt('123'), 'empresa_id' => 1, 'tipo_login' => 'Usuário','status' => true],
+            [ 'name' => 'Rafael Almeida', 'email' => 'rafael@gmail.com', 'password' => bcrypt('123'),'empresa_id' => 2, 'tipo_login' => 'Administrador', 'status' => true],
+            [ 'name' => 'Leandro William', 'email' => 'bahia@gmail.com', 'password' => bcrypt('123'), 'empresa_id' => 3, 'tipo_login' => 'Usuário','status' => true],
+            [ 'name' => 'João da Rosa', 'email' => 'mil@gmail.com', 'password' => bcrypt('123'), 'empresa_id' => 4, 'tipo_login' => 'Usuário','status' => true],
         ]);
     }
 }
